082423634437478b47ef3a1b91587ed36aa92c09
evaldo <evaaldormj@gmail.com>
2025-02-23 12:58:59 -0300

    Feat: Add card component and pipe

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IUser } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
+  imports: [CardComponent],
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
