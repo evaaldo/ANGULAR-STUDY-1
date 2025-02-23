85368551efc47c7b5faf145ad45a4f27ffb8eee5
evaldo <evaaldormj@gmail.com>
2025-02-22 22:31:57 -0300

    Feat: Add create task function

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { INewTask } from './new-task/new-task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -50,6 +51,18 @@ export class TasksComponent {
   }
 
   onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: INewTask) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    });
+
     this.isAddingTask = false;
   }
 }
